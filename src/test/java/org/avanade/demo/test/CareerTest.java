e722f76ea2620ee8b46089245c1c89a8812e9a7f
aleksander.gogoc <aleksander.gogoc@hotmail.com>
2018-12-15 16:34:57 +0100

    Fixed condition checking in test for Denmark

--- src/test/java/org/avanade/demo/test/CareerTest.java
+++ src/test/java/org/avanade/demo/test/CareerTest.java
@@ -14,18 +14,18 @@ public class CareerTest extends TestBase {
     @Test(enabled = true, retryAnalyzer = RetryFailed.class, groups = {"default"})
     public void testMoreThan10ResultsForCanada() {
         String country = "Canada";
-        int expectedMinValue = 10;
+        int expectedMinValue = 11;
 
         RolesAndLocationsPage rolesAndLocationsPage = navigateToRolesAndLocationsPage();
         CareerSearchResults careerSearchResults =
                 rolesAndLocationsPage
                         .selectLocation(country)
                         .clickSearchButton();
-        assertTrue(careerSearchResults.getResultCount() > expectedMinValue, "Search result number for Canada should be more than 10!");
+        assertTrue(careerSearchResults.getResultCount() >= expectedMinValue, "Search result number for Canada should be more than 10!");
     }
 
     @Test(enabled = true, retryAnalyzer = RetryFailed.class, groups = {"default"})
-    public void testMoreThan1ResultsForDenmark() {
+    public void testMoreThan0ResultsForDenmark() {
         String country = "Denmark";
         int expectedMinValue = 1;
 
@@ -34,14 +34,14 @@ public class CareerTest extends TestBase {
                 rolesAndLocationsPage
                         .selectLocation(country)
                         .clickSearchButton();
-        assertTrue(careerSearchResults.getResultCount() > expectedMinValue, "Search result number for Denmark should be more than 1!");
+        assertTrue(careerSearchResults.getResultCount() >= expectedMinValue, "Search result number for Denmark should be more than 1!");
     }
 
     @Test(enabled = true, retryAnalyzer = RetryFailed.class, groups = {"default"})
     public void testQualificationForJobOfferRaleigh() {
         String country = "United States";
         String city = "Raleigh";
-        String job = "QA Test";
+        String job = "QA Tester";
         String expectedQualification = "Experience with Agile/Scrum methodology";
 
         RolesAndLocationsPage rolesAndLocationsPage = navigateToRolesAndLocationsPage();
